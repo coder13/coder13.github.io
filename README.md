9b7578fb9e41817f1be8a651bb431558c8d13d7e
Choover <choover11@gmail.com>
2022-11-08 09:44:07 -0800

    Fixed metadata; added competitions page

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ This is my personal portfolio / blog website.
 It's built with jekyll and has dynamic content powered by vuejs
 
 The vue code in `main.js` fetches from github to autofill project data and update the stars, language, and description on my repos so far. My plan is to hopefully automate the entirety of it so that I only need to update a description in one place for example.
+
+## Installing
+
+```bash
+gem install jekyll bundler
+```
+
+## Running
+
+```bash
+bundle exec jekyll serve
+```
--- assets/main.js
+++ assets/main.js
@@ -1,49 +1,74 @@
-const GITHUB_URL = 'https://api.github.com';
+const GITHUB_URL = "https://api.github.com";
+const WCA_URL = "https://www.worldcubeassociation.org/api/v0";
 
 new Vue({
-  el: '#app',
-  data () {
+  el: "#app",
+  data() {
     return {
-      foo: 'bar',
-      projects: [{
-        name: 'LetsCube',
-        url: 'https://github.com/coder13/letscube',
-        website: 'https://letscube.net',
-        repo: 'letscube',
-        docs: true,
-        data: {},
-      }, {
-        name: 'Carat',
-        url: 'https://github.com/coder13/carat',
-        repo: 'carat',
-        data: {},
-      }, {
-        name: 'Fantasy Cubing',
-        url: 'https://github.com/coder13/fantasycubing',
-        repo: 'fantasycubing',
-        data: {},
-      }],
-      github: {
-        avatar_url: 'https://via.placeholder.com/460'
+      foo: "bar",
+      projects: [
+        {
+          name: "LetsCube",
+          url: "https://github.com/coder13/letscube",
+          website: "https://letscube.net",
+          repo: "letscube",
+          docs: true,
+          data: {},
+        },
+        {
+          name: "Carat",
+          url: "https://github.com/coder13/carat",
+          repo: "carat",
+          data: {},
+        },
+        {
+          name: "Fantasy Cubing",
+          url: "https://github.com/coder13/fantasycubing",
+          repo: "fantasycubing",
+          data: {},
+        },
+      ],
+      wca: {
+        me: {},
+        competitions: [],
       },
-    }
+      github: {
+        avatar_url: "https://via.placeholder.com/460",
+      },
+    };
   },
-  created () {
+  created() {
     // fetch the data when the view is created and the data is
     // already being observed
     this.fetchData();
   },
   methods: {
     fetchData() {
-      fetch(`${GITHUB_URL}/users/coder13`).then((data) => data.json()).then((data) => {
-        this.github = data;
-      });
+      fetch(`${GITHUB_URL}/users/coder13`)
+        .then((data) => data.json())
+        .then((data) => {
+          this.github = data;
+        });
 
       this.projects.forEach((project) => {
-        fetch(`${GITHUB_URL}/repos/coder13/${project.repo}`).then((data) => data.json()).then((data => {
-          project.data = data;
-        }));
-      })
-    }
-  }
+        fetch(`${GITHUB_URL}/repos/coder13/${project.repo}`)
+          .then((data) => data.json())
+          .then((data) => {
+            project.data = data;
+          });
+      });
+
+      fetch(`${WCA_URL}/users/8184?upcoming_competitions=true`)
+        .then((data) => data.json())
+        .then((data) => {
+          console.log(data);
+          this.wca.me = data.user;
+          this.wca.competitions = data.upcoming_competitions.sort(
+            (a, b) =>
+              new Date(a.start_date).getTime() -
+              new Date(b.start_date).getTime()
+          );
+        });
+    },
+  },
 });
